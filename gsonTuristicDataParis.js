286651ce4f92e5240b8ad33166230d77e241759b
Claudia Munoz <cmunoz@pros.com>
2022-05-15 22:36:09 -0400

    EM-004: add turistic places and recomended turistic route

---
+++ gsonTuristicDataParis.js
@@ -0,0 +1,118 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.294495487888611,48.85821241904537]
+        },
+        properties: {
+          title: 'Tour Eiffel.',
+          description: 'Torre del siglo XIX de 324 m de altura.',
+          image:'./images/Eiffel.png',
+          width: '59px',
+          height: '100px'
+        }
+      },      
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.33640812735203,48.84683850318794]
+        },
+        properties: {
+          title: 'Le jardin du Luxembourg',
+          description: 'Parques con prados elegantes y lindas estatuas',
+          image:'./images/jardinPalace.png',
+          width: '100px',
+          height: '76px'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.3499464259452054,48.85293527958197]
+        },
+        properties: {
+          title: 'Cathedrale Notre-Dame de Paris',
+          description: 'Catedral dedicada a Maria madre de Jesus, construida en 1164 y terminada en 1345',
+          image:'./images/notredame.png',
+          width: '93px',
+          height: '100px'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.3358113886960155,48.86104022136112]
+        },
+        properties: {
+          title: 'Musee du Louvre',
+          description: 'Emblematico y extenso museo de arte junto a la primaide del Lourvre',
+          image:'./images/louvre.png',
+          width: '100px',
+          height: '48px'
+        }
+      },      
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.326496829284565,48.85997622013548]
+        },
+        properties: {
+          title: 'Musee d\'Orsay',
+          description: 'Obras maestras de los siglos  XIX y XX',
+          image:'./images/dorsay.png',
+          width: '100px',
+          height: '42px'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.311633983436991,48.86883568258412]
+        },
+        properties: {
+          title: 'Elisee Palace et Jardins des Champs-Elysees',
+          description: 'Jardines del siblo XVII diseñados por le Notre',
+          image:'./images/jardinPalace.png',
+          width: '100px',
+          height: '70px'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.2950475992922463,48.873694650774524]
+        },
+        properties: {
+          title: 'Arc de Triomphe',
+          description: 'Arco triunfal y monumento Nacional',
+          image:'./images/archTriunfo.png',
+          width: '100px',
+          height: '87px'
+        }
+      },      
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [2.332419613002145,48.88407342299749]
+        },
+        properties: {
+          title: 'Moulin Rouge',
+          description: 'La Machine du Moulin Rouge',
+          image:'./images/moulinrouge.png',
+          width: '68px',
+          height: '100px'
+        }
+      }
+    ]
+  };
+  
--- mapanimation.js
+++ mapanimation.js
@@ -1,43 +1,173 @@
-// This array contains the coordinates for all bus stops between MIT and Harvard
-const busStops = [
-  [2.2958480933682495,48.87305727011196],
-  [2.296341807809881,48.87238551903741],
-  [2.297108239194415,48.871440691883805],
-  [2.2980560727687305,48.870045804891475],
-  [2.2988850154548004,48.86860783107147],
-  [2.2995119315283716,48.86667326483362],
-  [2.2998592401827977,48.86507498444831],
-  [2.301309044340286,48.86454215967541],
-  [2.3016231702733876,48.86249310156737],
-  [2.296992554306712,48.86150722262619],
-  [2.294059395030132,48.85949736216395],
-  [2.294495487888611,48.85821241904537],
-];
-
-// TODO: add your own access token
+// Add your own access token
 mapboxgl.accessToken =
-  '';
+  'pk.eyJ1IjoiY2xhdWRpYS1wYW9sYSIsImEiOiJjbDMyZTdvODQwM3AxM2lzOG02MHlvZnhhIn0.pFjq0r23WnlpQXMspTXCZA';
 
-// TODO: create the map object using mapboxgl.map() function
-let map = new mapboxgl.Map({
+// Create the map object using mapboxgl.map() function
+var cordinates = [];
+var remove = false;
+var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [2.2951363254344415,48.87357082378131],
-  
-  zoom: 13,
+  center: [2.3327100842398067,48.86455614176651],  
+  zoom: 13.3,
 });
 
-let marker = new mapboxgl.Marker().setLngLat([2.2951363254344415,48.87357082378131]).addTo(map);
+// Add the Turistic Places with personalize markers
+geojson.features.forEach(function(marker){
+  const el = document.createElement('div');
+  el.className = 'marker';
+  el.style.width = `${marker.properties.width}`;
+  el.style.height = `${marker.properties.height}`;
+  el.style.backgroundImage = `url(${marker.properties.image})`;  
+  cordinates.push(marker.geometry.coordinates);
+  new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+});
 
-// counter here represents the index of the current bus stop
-let counter = 0;
-function move() {
-  // move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-  // Use counter to access bus stops in the array busStops
-  setTimeout(() => {
-    if (counter >= busStops.length) return;
-    marker.setLngLat(busStops[counter]);
-    counter++;
-    move();
-  }, 1000);
+
+
+// Add and Customize thd draw tool
+var draw = new MapboxDraw({
+  displayControlsDefault: false,
+  controls: {
+    line_string: true,
+    trash: true
+  },
+
+  styles: [
+    // ACTIVE (being drawn)
+    // line stroke
+    {
+      "id": "gl-draw-line",
+      "type": "line",
+      "filter": ["all", ["==", "$type", "LineString"], ["!=", "mode", "static"]],
+      "layout": {
+        "line-cap": "round",
+        "line-join": "round"
+      },
+      "paint": {
+        "line-color": "#FF3333",
+        "line-dasharray": [0.2, 2],
+        "line-width": 8,
+        "line-opacity": 0.7
+      }
+    },
+    // vertex point halos
+    {
+      "id": "gl-draw-polygon-and-line-vertex-halo-active",
+      "type": "circle",
+      "filter": ["all", ["==", "meta", "vertex"], ["==", "$type", "Point"], ["!=", "mode", "static"]],
+      "paint": {
+        "circle-radius": 10,
+        "circle-color": "#FFF"
+      }
+    },
+    // vertex points
+    {
+      "id": "gl-draw-polygon-and-line-vertex-active",
+      "type": "circle",
+      "filter": ["all", ["==", "meta", "vertex"], ["==", "$type", "Point"], ["!=", "mode", "static"]],
+      "paint": {
+        "circle-radius": 6,
+        "circle-color": "#3b9ddd",
+      }
+    },
+  ]
+});
+// add the draw tool to the map
+map.addControl(draw);
+
+// use the coordinates you just drew to make your directions request
+function updateRoute() {
+  removeRoute(); // overwrite any existing layers
+  var data = draw.getAll();  
+  //console.log(data);
+  var lastFeature = data.features.length - 1;
+  var coords = data.features[lastFeature].geometry.coordinates;
+  console.log(coords);
+  var newCoords = coords.join(';')
+
+  console.log(newCoords);
+
+  getMatch(newCoords);
 }
+function recomendedRoute(){
+  let route = document.getElementById("route");
+  
+  if (remove === false){
+    route.innerHTML = "Remove Recommended Turistic Route";
+    remove = true;
+  }else {    
+    route.innerHTML = "Recommended Turistic Route";
+    remove = false;
+  }
+  console.log(cordinates.join(';'));
+  getMatch(cordinates.join(';'));
+}
+
+// make a directions request
+function getMatch(e) {
+  // https://www.mapbox.com/api-documentation/#directions
+  var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + e +'?geometries=geojson&steps=true&&access_token=' + mapboxgl.accessToken;
+  var req = new XMLHttpRequest();
+  req.responseType = 'json';
+  req.open('GET', url, true);
+  req.onload  = function() {
+    var jsonResponse = req.response;
+    //console.log(jsonResponse);
+    var distance = jsonResponse.routes[0].distance*0.001; // convert to km
+    var duration = jsonResponse.routes[0].duration/60; // convert to minutes
+    // add results to info box
+    document.getElementById('calculated-line').innerHTML = '<strong>Distance:</strong> ' + distance.toFixed(2) + ' km<br> <strong>Duration:</strong> ' + duration.toFixed(2) + ' minutes';
+    var coords = jsonResponse.routes[0].geometry;
+    // add the route to the map
+    addRoute(coords);
+  };
+  req.send();
+}
+
+// adds the route as a layer on the map
+function addRoute (coords) {
+  // check if the route is already loaded
+  if (map.getSource('route')) {
+    map.removeLayer('route')
+    map.removeSource('route')
+  } else{
+    map.addLayer({
+      "id": "route",
+      "type": "line",
+      "source": {
+        "type": "geojson",
+        "data": {
+          "type": "Feature",
+          "properties": {},
+          "geometry": coords
+        }
+      },
+      "layout": {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      "paint": {
+        "line-color": "#850707",
+        "line-width": 8,
+        "line-opacity": 0.8
+      }
+    });
+  };
+}
+
+// remove the layer if it exists
+function removeRoute () {
+  if (map.getSource('route')) {
+    map.removeLayer('route');
+    map.removeSource('route');
+    document.getElementById('calculated-line').innerHTML = '';
+  } else  {
+    return;
+  }
+}
+
+// add create, update, or delete actions
+map.on('draw.create', updateRoute);
+map.on('draw.update', updateRoute);
+map.on('draw.delete', removeRoute);
